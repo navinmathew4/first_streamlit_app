acc7d726cc8f2754fb8a1f98b6d0efea7bb1b543
navinmathew4 <78461824+navinmathew4@users.noreply.github.com>
2022-08-20 18:28:28 +0530

    Update streamlit_app.py
    added emojis

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,8 @@ import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
 streamlit.header('Breakfast Menu')
-streamlit.text('Idli')
-streamlit.text('Vada')
-streamlit.text('Poha')
+streamlit.text('🥣Idli')
+streamlit.text('🍞Vada')
+streamlit.text('🥗Poha')
+ 
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
