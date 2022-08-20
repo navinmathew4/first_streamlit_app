381bed03052c8980cd02a8044a3472cc047681a9
navinmathew4 <78461824+navinmathew4@users.noreply.github.com>
2022-08-20 18:37:41 +0530

    Update streamlit_app.py
    pandas used to display list form csv

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit
-
+import pandas
 streamlit.title('My Parents New Healthy Diner')
 streamlit.header('Breakfast Menu')
 streamlit.text('🥣Idli')
@@ -7,3 +7,5 @@ streamlit.text('🍞Vada')
 streamlit.text('🥗Poha')
  
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
